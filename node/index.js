f882358d932bd6e3034ae29e827d4f7882f8af4e
Rishika Gupta <rishikagupta246813579@gmail.com>
2020-08-08 00:00:47 +0530

    backend for report new, edit, delete function

--- node/index.js
+++ node/index.js
@@ -8,6 +8,12 @@ var express = require('express'),
 const multer = require('multer');
 var cloudinary = require('cloudinary').v2;
 
+cloudinary.config({
+  cloud_name: 'dk96tpgwo',
+  api_key: '257327353339548',
+  api_secret: 'c4ItASdO3ykmYH5T5U8Ga2q-VBM'
+});
+
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, './public/upload')
@@ -38,16 +44,14 @@ const db = new Firestore({
 app.get("/",function(req,res){
     res.render("profile");
 });
-app.get("/reports/new",function(req,res){
+app.get("/report/new",function(req,res){
     res.render("new");
 });
-app.get("/reports",async function(req,res){
-    const snapshot = await db.collection('reports').get();
-    snapshot.forEach((doc) => {
-        console.log(doc.id, '=>', doc.data());
-    });
+app.get("/report",async function(req,res){
+    const reports = await db.collection('reports').get();
+    res.render("show",{reports: reports});
 });
-app.post("/reports", upload.single('image'), async function(req, res){
+app.post("/report", upload.single('image'), async function(req, res){
     var desc = req.body.description;
     const image = await cloudinary.uploader.upload("./public/upload/save.jpg");
     db.collection("reports").add({
@@ -55,11 +59,47 @@ app.post("/reports", upload.single('image'), async function(req, res){
         description: desc
     })
     .then(function(newReport) {
-        res.redirect("/reports");
+        res.redirect("/report");
     })
     .catch(function(error) {
         console.log(error);
         res.redirect("/");
+    });
+});
+
+app.get('/report/:id/edit', function (req, res) {
+    var report = db.collection("reports").doc(req.params.id);
+    report.get().then(function(report) {
+        if (report.exists) {
+            res.render("edit", {report:report});
+        } else {
+            console.log("No such document!");
+            res.redirect("/");
+        }
+    }).catch(function(error) {
+        console.log("Error getting document:", error);
+        res.redirect("/");
+    });
+});
+
+// UPDATE Route
+app.put('/report/:id',upload.single('image'), async function (req, res) {
+    var desc = req.body.description;
+    const image = await cloudinary.uploader.upload("./public/upload/save.jpg");
+    db.collection("reports").doc(req.params.id).update({
+        "img": image.secure_url,
+        "description": desc
+    });
+    res.redirect("/report");
+});
+
+//DELETE Route
+app.delete('/report/:id', function (req, res) {
+    db.collection("reports").doc(req.params.id).delete().then(function() {
+        res.redirect('/report');
+    }).catch(function(error) {
+        console.log(error);
+        res.redirect('/report');
     });
 });
 
