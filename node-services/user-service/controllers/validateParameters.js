e9b3b591cb31c659c665510a89a65377e634e1d2
Ankit Verma <verma.ankit484@gmail.com>
2020-08-20 19:22:39 +0530

    User service (#5)
    * initiate user-service
    
    * add user signup route/service
    
    * add login route/service
    
    * delete old node service
    
    * minor changes
    
    * update gitignore

---
+++ node-services/user-service/controllers/validateParameters.js
@@ -0,0 +1,16 @@
+const validate = {
+  validateEmail: (email) => {
+    const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return regex.test(String(email).toLowerCase());
+  },
+
+  validateUserParameter: (string) => {
+    if (!string || string.length === 0) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+}
+
+module.exports = validate;
