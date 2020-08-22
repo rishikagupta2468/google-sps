13eb66069211a6b8749ec7b1f07cbfc8a94e1a70
Ankit Verma <verma.ankit484@gmail.com>
2020-08-22 08:38:04 +0000

    add email parameter on rendering bmi page

--- frontend/routes/bmi-calculator.js
+++ frontend/routes/bmi-calculator.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router  = express.Router();
 
 router.get('/', async function (req, res) {
-     res.render("bmi-calculator");  
+  res.render("bmi-calculator", {
+    email: req.email
+  });  
 });
 module.exports = router;
